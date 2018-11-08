60f50731af3d1af2875805e1cfbd38aa2821a72d
pawel <developerpi91@gmail.com>
2018-11-08 20:56:52 +0100

    add divide function

--- Lesson8_rzutowanie/src/pl/developerpi91/rzutowanie/Main.java
+++ Lesson8_rzutowanie/src/pl/developerpi91/rzutowanie/Main.java
@@ -6,7 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		MultipleByPi a = new MultipleByPi();
-		a.run();
+//		a.run();
+		a.run2();
 	}
 
 }
--- Lesson8_rzutowanie/src/pl/developerpi91/rzutowanie/MultipleByPi.java
+++ Lesson8_rzutowanie/src/pl/developerpi91/rzutowanie/MultipleByPi.java
@@ -6,14 +6,19 @@ import java.math.*;
 public class MultipleByPi {
 	Scanner sc = new Scanner(System.in);
 	private int inputValue;
-	public void getInputValue() {
-		try {
-			this.inputValue = sc.nextInt();
-			
-		} catch (InputMismatchException e) {
-			System.out.println("Input value is not integer");
-			sc.next();
-		}
+	private int inputValue2;
+	public int getInputValue() {
+		int input;
+		while(true) {
+			try {
+					input = sc.nextInt();
+					break;
+			} catch (InputMismatchException e) {
+				System.out.println("Input value is not integer");
+				sc.next();
+			}		
+	}
+		return input;
 	}
 	public void multipleByPi() {
 		int resultInt = this.inputValue * (int)Math.PI;
@@ -21,8 +26,20 @@ public class MultipleByPi {
 		System.out.println(resultInt);
 		System.out.println(resultDouble);
 	}
+	public void divide() {
+		System.out.println(inputValue);
+		System.out.println(inputValue2);
+
+		System.out.println((double)inputValue/(double)inputValue2);
+	}
 	public void run() {
-		getInputValue();
+		this.inputValue=getInputValue();
 		multipleByPi();
 	}
+	public void run2() {
+		this.inputValue=getInputValue();
+		this.inputValue2=getInputValue();
+		divide();
+	}
+	
 }
