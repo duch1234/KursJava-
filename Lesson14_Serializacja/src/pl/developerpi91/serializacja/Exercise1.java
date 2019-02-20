1421e29b720da5ec90288a7895fd051af8a3717a
pawel <developerpi91@gmail.com>
2019-02-20 22:08:14 +0100

    Lesson 13 Exercise 1 finished

--- Lesson14_Serializacja/src/pl/developerpi91/serializacja/Exercise1.java
+++ Lesson14_Serializacja/src/pl/developerpi91/serializacja/Exercise1.java
@@ -1,6 +1,6 @@
 package pl.developerpi91.serializacja;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Exercise1 implements Serializable {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         String currentName;
         List<String> names = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
@@ -22,10 +22,27 @@ public class Exercise1 implements Serializable {
             }
         }
         System.out.println(names);
-    }
-    public void serialize(){
-        //TODO
 
+        try{
+            serialize(names);
+            deSerialize(names);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void serialize(List<String> names)throws IOException{
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("names.txt"));
+        oos.writeObject(names);
+    }
+
+    public static void deSerialize(List<String> names)throws IOException, ClassNotFoundException{
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("names.txt"));
+        List<String> namesReaded = null;
+        namesReaded=(List<String>)ois.readObject();
+        for (String element:namesReaded) {
+            System.out.println("readed element " + element);
+        }
+        System.out.println(namesReaded);
     }
 
 }
